d6d141dd6557b09ca8a26fdb40bf8f3393458107
tianpratama1109 <55422336+tianpratama1109@users.noreply.github.com>
2019-10-10 09:33:15 -0500

    Update followkeyword.php

--- followkeyword.php
+++ followkeyword.php
@@ -9,19 +9,19 @@ define('access_token', '#ATLOE');
 define('access_token_secret', '#ATSLOE');
 
 $jumlah = "1";
-$keyword ="Target Loe"; #example askmenfess
+$keyword ="Target Loe"; #example askmenfess DMenfess dll
 $type = "live";
 
 // post tweet
 $koneksi = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, access_token, access_token_secret);
-$nasi = $koneksi->get('search/tweets', array('q' => $keyword, 'count' => $jumlah));
-$statuses = $nasi->statuses;
+$crot = $koneksi->get('search/tweets', array('q' => $keyword, 'count' => $jumlah));
+$statuses = $crot->statuses;
 foreach($statuses as $status)
 {
 $username = $status->user->screen_name;
 $text = $status->text;
 $idstr = $status->id_str;
-$koneksi->post('friendships/create', array('screen_name' => $username));
+$eksekusi = $koneksi->post('friendships/create', array('screen_name' => $username));
 
 if($eksekusi->errors) {
 echo "<center>Gagal</center>";
