bcae595e652db1d84962d4630062a5222cfc21a6
tianpratama1109 <55422336+tianpratama1109@users.noreply.github.com>
2019-10-11 08:17:57 -0500

    Update followkeyword.php

--- followkeyword.php
+++ followkeyword.php
@@ -9,14 +9,12 @@ define('access_token', '#ATLOE');
 define('access_token_secret', '#ATSLOE');
 
 $jumlah = "1";
-
 function randomline( $target )
 {
     $lines = file( $target );
     return $lines[array_rand( $lines )];
 }
 $target = randomline('target.txt');
-
 $koneksi = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, access_token, access_token_secret);
 $nasi = $koneksi->get('search/tweets', array('q' => $target,  'count' => $jumlah, 'result_type' => recent));
 $statuses = $nasi->statuses;
@@ -24,7 +22,6 @@ foreach($statuses as $status)
 {
 $username = $status->user->screen_name;
 $koneksi->post('friendships/create', array('screen_name' => $username));
-
 if($eksekusi->errors) {
 echo "<center>Gagal</center>";
 }
